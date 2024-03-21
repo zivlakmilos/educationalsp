3025d300626c419eaba3361f182f8c20790049ec
Milos Zivlak <zivlakmilos@gmail.com>
2024-03-22 00:26:52 +0100

    Initialize message

--- cmd/educationalsp/main.go
+++ cmd/educationalsp/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"log"
 	"os"
 
+	"github.com/zivlakmilos/educationalsp/pkg/lsp"
 	"github.com/zivlakmilos/educationalsp/pkg/rpc"
 )
 
@@ -16,13 +18,39 @@ func main() {
 	scanner.Split(rpc.Split)
 
 	for scanner.Scan() {
-		msg := scanner.Text()
-		handlMessage(logger, msg)
+		msg := scanner.Bytes()
+		method, contents, err := rpc.DecodeMessage(msg)
+		if err != nil {
+			logger.Printf("error: %s\n", err)
+			continue
+		}
+
+		handlMessage(logger, method, contents)
 	}
 }
 
-func handlMessage(logger *log.Logger, msg any) {
-	logger.Println(msg)
+func handlMessage(logger *log.Logger, method string, contents []byte) {
+	logger.Printf("received msg with method: %s\n", method)
+
+	switch method {
+	case "initialize":
+		var request lsp.InitializeRequest
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("error parsing: %s\n", err)
+		}
+
+		logger.Printf("connected to: %s %s",
+			request.Params.ClientInfo.Name,
+			request.Params.ClientInfo.Version)
+
+		msg := lsp.NewInitializeResponse(request.ID)
+		reply := rpc.EncodeMessage(msg)
+
+		writer := os.Stdout
+		writer.Write([]byte(reply))
+
+		logger.Printf("sent reply")
+	}
 }
 
 func getLogger(filename string) *log.Logger {
