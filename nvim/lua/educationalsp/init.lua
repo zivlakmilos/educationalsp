39c7367ae151f3e334ecfe6e9df514b512bd6928
Milos Zivlak <zivlakmilos@gmail.com>
2024-03-24 04:52:05 +0100

    Hover keybinding for lsp

--- nvim/lua/educationalsp/init.lua
+++ nvim/lua/educationalsp/init.lua
@@ -10,7 +10,9 @@ end
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "markdown",
-	callback = function()
+	callback = function(event)
+		print("usao")
 		vim.lsp.buf_attach_client(0, client)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf })
 	end,
 })
